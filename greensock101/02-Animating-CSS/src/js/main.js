7c9ee95f0aa1771a49319ff543ca057786eb08b3
Matthew Choi <matthewchoi7@gmail.com>
2017-08-11 11:36:23 +1000

    G101 - Controlling Timeline Playback

--- greensock101/02-Animating-CSS/src/js/main.js
+++ greensock101/02-Animating-CSS/src/js/main.js
@@ -16,11 +16,49 @@
 	// TweenLite.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 1});
 
 	tl
-		.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
+		.from(h1, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
 		.add('intro')
-		.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
-		.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, 'intro')
-		.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, 'intro+=3')
-		.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, 'intro');
+		.from(intro, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
+		.from(img, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
+		.from(h2, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
+		.from(listItem, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut});
+
+	tl.pause();
+
+	$('#btnPlay').on('click', function() {
+		tl.play();
+	});
+
+	$('#btnPause').on('click', function() {
+		tl.pause();
+	});
+
+	$('#btnResume').on('click', function() {
+		tl.resume();
+	});
+
+	$('#btnReverse').on('click', function() {
+		tl.reverse();
+	});
+
+	$('#btnSpeedUp').on('click', function() {
+		tl.timeScale(10);
+	});
+
+	$('#btnSlowDown').on('click', function() {
+		tl.timeScale(0.5);
+	});
+
+	$('#btnSeek').on('click', function() {
+		tl.seek(1);
+	});
+
+	$('#btnProgress').on('click', function() {
+		tl.progress(0.5);
+	});
+
+	$('#btnRestart').on('click', function() {
+		tl.restart();
+	});
 
 })(jQuery);
