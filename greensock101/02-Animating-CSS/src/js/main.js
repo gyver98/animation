7cb2d58ae45884a898c15ececdd25a7f2ea92e8b
Matthew Choi <matthewchoi7@gmail.com>
2017-08-11 10:42:59 +1000

    G101 - Animating Multiple Objects

--- greensock101/02-Animating-CSS/src/js/main.js
+++ greensock101/02-Animating-CSS/src/js/main.js
@@ -1,29 +1,17 @@
 (function($) {
     
 	var img = $('img'),
-			h2 = $('h2');
-			i = 0;
+			h2 = $('h2'),
+			h1 = $('h1'),
+			intro = $('.intro'),
+			listItem = $('ul li');
+
 
 	// Simple Tween
-	TweenLite.from(img, 1, {
-		x: -200, 
-		ease: Power1.easeInOut, 
-		onStart: onStart,
-		onUpdate: onUpdate,
-		onComplete: onComplete
-	});
-
-	function onStart() {
-		console.log("animation started");
-	}
-
-	function onUpdate() {
-		h2.text("loading...");
-	}
-
-	function onComplete() {
-		h2.text("loaded");
-	}
-	
+	TweenLite.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 0.2});
+	TweenLite.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 0.4});
+	TweenLite.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 0.6});
+	TweenLite.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 0.8});
+	TweenLite.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 1});
 
 })(jQuery);
