5c65e0bd655e9cede7d32f7d7872f74611e5990a
Matthew Choi <matthewchoi7@gmail.com>
2017-08-11 11:47:28 +1000

    G101 - Staggering Animations

--- greensock101/02-Animating-CSS/src/js/main.js
+++ greensock101/02-Animating-CSS/src/js/main.js
@@ -5,6 +5,7 @@
 			h1 = $('h1'),
 			intro = $('.intro'),
 			listItem = $('ul li'),
+			buttons = $('button'),
 			tl = new TimelineLite();
 
 
@@ -16,14 +17,15 @@
 	// TweenLite.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut, delay: 1});
 
 	tl
-		.from(h1, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
-		.add('intro')
-		.from(intro, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
-		.from(img, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
-		.from(h2, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
-		.from(listItem, 1, {y:-15, autoAlpha: 0, ease:Power1.easeInOut});
-
-	tl.pause();
+		.from(h1, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut})
+		.from(intro, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, '-=0.15')
+		.from(img, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, '-=0.15')
+		.from(h2, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, '-=0.15')
+		.from(listItem, 0.3, {y:-15, autoAlpha: 0, ease:Power1.easeInOut}, '-=0.15')
+		.staggerFromTo(buttons, 0.2, 
+			{autoAlpha: 0, x: 10}, 
+			{x: -20, autoAlpha: 1, ease:Power1.easeOut}, 
+			0.1);
 
 	$('#btnPlay').on('click', function() {
 		tl.play();
